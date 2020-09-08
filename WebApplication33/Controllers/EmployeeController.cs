9ddff5ac43983e9981d16e20c47f06b8d7125f73
Zainab Saify <zainab.saify@dxc.com>
2020-09-08 15:17:51 +0530

    add home view

--- WebApplication33/Controllers/EmployeeController.cs
+++ WebApplication33/Controllers/EmployeeController.cs
@@ -13,5 +13,10 @@ namespace WebApplication33.Controllers
         {
             return View();
         }
+        public ActionResult Home()
+        {
+            return View();
+        }
+
     }
 }
